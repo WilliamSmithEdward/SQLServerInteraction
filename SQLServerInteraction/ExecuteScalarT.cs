bef02f54916e6fcd0443ee1e1b812b5f8f9179d9
William Smith <williamsmithe@icloud.com>
2023-09-25 00:00:03 -0700

    Additional xml documentation and unit testing

--- SQLServerInteraction/ExecuteScalarT.cs
+++ SQLServerInteraction/ExecuteScalarT.cs
@@ -4,6 +4,12 @@ namespace SQLServerInteraction
 {
     public partial class SQLServerInstance
     {
+        /// <summary>
+        /// Executes a SQL query and returns the result as a single value of type T.
+        /// </summary>
+        /// <typeparam name="T">The type of the expected result.</typeparam>
+        /// <param name="sql">The SQL query to execute.</param>
+        /// <returns>The result of the query as a single value of type T, or the default value of T if the result is null or DBNull.Value.</returns>
         public T? ExecuteScalar<T>(string sql)
         {
             using var connection = new SqlConnection(_connectionString);
--- SQLServerInteraction/ExecuteScalarTAsync.cs
+++ SQLServerInteraction/ExecuteScalarTAsync.cs
@@ -4,6 +4,12 @@ namespace SQLServerInteraction
 {
     public partial class SQLServerInstance
     {
+        /// <summary>
+        /// Asynchronously executes a SQL query and returns the result as a single value of type T.
+        /// </summary>
+        /// <typeparam name="T">The type of the expected result.</typeparam>
+        /// <param name="sql">The SQL query to execute.</param>
+        /// <returns>A task representing the asynchronous operation that returns the result of the query as a single value of type T, or the default value of T if the result is null or DBNull.Value.</returns>
         public async Task<T?> ExecuteScalarAsync<T>(string sql)
         {
             using var connection = new SqlConnection(_connectionString);
